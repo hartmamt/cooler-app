48bfbb4f283ef35672bb0d1721dce85079847b19
Matthew Hartman <mattandalissa@gmail.com>
2017-08-15 09:03:42 -0400

    Update MeasureLine.jsx

--- src/components/MeasureLine.jsx
+++ src/components/MeasureLine.jsx
@@ -2,22 +2,6 @@ import React from 'react';
 import { Line, Group, Rect, Text } from 'react-konva';
 
 export const MeasureLine = ({ x, y, x1, y1, orientation, text }) => {
-  // if (line.t.substring(line.t.length - 5) == ' 0 in') {
-  //   ctx.fillRect((line.x1 - line.x0) / 2 + line.x0, line.y0 - 5, 40, 10);
-  //   ctx.fillStyle = color;
-  //   ctx.fillText(line.t.substr(0, 5), (line.x1 - line.x0) / 2 + line.x0 + 10, line.y0 + 5);
-  // } else {
-  //   ctx.fillRect((line.x1 - line.x0) / 2 + line.x0, line.y0 - 5, 60, 10);
-  //   ctx.fillStyle = color;
-  //   ctx.fillText(line.t, (line.x1 - line.x0) / 2 + line.x0 + 10, line.y0 + 5);
-  // }
-  //        ctx.fillRect(line.x0 - 5,((line.y1 - line.y0) / 2) + line.y0, 20, 15)
-  // ctx.fillStyle = color;
-  // if (line.t.substring(line.t.length - 5) == ' 0 in') {
-  //     ctx.fillText(line.t.substr(0,5), line.x0 - 10, ((line.y1 - line.y0) / 2) + line.y0 + 10);
-  // } else {
-  //     ctx.fillText(line.t, line.x0 - 10, ((line.y1 - line.y0) / 2) + line.y0 + 10);
-  // }
   const coordinates =
     orientation === 'Horizontal'
       ? {
